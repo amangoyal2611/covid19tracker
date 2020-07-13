9149e247fe73771bf547404710458a3558fa5ff0
Aman Goyal <im.developer26@gmail.com>
2020-07-13 14:07:36 +0530

    Updates

---
+++ precache-manifest.8f0ee489f355368a16e4f6adf7a66be5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c5720c2f2e514acc4d5a61f1f0616d",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "4f3cbfb029782eb0fae2",
+    "url": "/covid19tracker/static/css/main.114b9c78.chunk.css"
+  },
+  {
+    "revision": "375d8374fc736698abfe",
+    "url": "/covid19tracker/static/js/2.05b8f8de.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.05b8f8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3cbfb029782eb0fae2",
+    "url": "/covid19tracker/static/js/main.a3ca0f46.chunk.js"
+  },
+  {
+    "revision": "cec97389c754e0ae4d2e",
+    "url": "/covid19tracker/static/js/runtime-main.169bc779.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19tracker/static/media/covid19image.d7265326.png"
+  }
+]);
